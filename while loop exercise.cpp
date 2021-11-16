8ffe57f85cac00fe1b66bc57b28bf2fd586b28b2
KelvinLemanta <85560626+KelvinLemanta@users.noreply.github.com>
2021-11-16 20:07:08 +0300

    Add files via upload

---
+++ while loop exercise.cpp
@@ -0,0 +1,19 @@
+
+
+// while loop to get sup of numbers between 20 to 25
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int counter = 20;
+	int sum = 0;
+	while (counter<=25)
+	{
+		sum+=counter;
+		counter++;
+	}
+	cout<<"sum is:"<<sum;
+	return 0;
+}
